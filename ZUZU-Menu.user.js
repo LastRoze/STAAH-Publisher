ba656b597a7af4c14c7dabde7728a7f360f669b5
Yoga <LastRoze@users.noreply.github.com>
2019-11-08 07:57:25 +0700

    Update ZUZU-Menu.user.js

--- ZUZU-Menu.user.js
+++ ZUZU-Menu.user.js
@@ -1,26 +1,26 @@
 // ==UserScript==
-// @name		    STAAH Publisher
-// @namespace	    https://linkedin.com/in/lastroze
-// @author		    Last Roze
-// @description     ZUZU Hospitality - STAAH publisher is created to easily publish rates across OTA with one click.
-// @icon		    https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
-// @version		    1.0
-// @copyright		2019, Yoga Budiman
-// @grant		    GM_addStyle
-// @grant           GM_deleteValue
-// @grant           GM_getResourceURL
-// @grant           GM_getValue
-// @grant           GM_openInTab
-// @grant           GM_registerMenuCommand
-// @grant           GM_setValue
-// @grant           GM_xmlhttpRequest
-// @grant           GM.deleteValue
-// @grant           GM.getResourceUrl
-// @grant           GM.getValue
-// @grant           GM.openInTab
-// @grant           GM.xmlHttpRequest
-// @grant           unsafeWindow
-// @match		    *://cp.staah.net/common-cgi/motels/admin/ChannelUpdate.pl*
+// @name         STAAH Publisher
+// @namespace    https://linkedin.com/in/lastroze
+// @author       Last Roze
+// @description  ZUZU Hospitality - STAAH publisher is created to easily publish rates across OTA with one click.
+// @icon         https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
+// @version      1.1
+// @copyright		 ©2019 - Yoga Budiman
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.deleteValue
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @match        *://cp.staah.net/common-cgi/motels/admin/ChannelUpdate.pl*
 // @noframes
 // ==/UserScript==
 
