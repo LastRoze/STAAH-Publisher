dbdc7ade71022a8e32fdc8e872f49d167ad18603
Yoga <LastRoze@users.noreply.github.com>
2019-11-09 03:18:20 +0700

    Create ZUZU-Menu.meta.js

---
+++ ZUZU-Menu.meta.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         STAAH Publisher
+// @namespace    STAAH Publisher
+// @description  ZUZU Hospitality - STAAH Publisher is created to easily publish rates across OTA with one click.
+// @copyright    ©2019 - Yoga Budiman
+// @version      1.2
+// @author       Last Roze
+// @homepageURL  https://github.com/LastRoze/
+// @supportURL   https://linkedin.com/in/lastroze
+// @updateURL    https://github.com/LastRoze/STAAH-Publisher/raw/master/ZUZU-Menu.meta.js
+// @downloadURL  https://github.com/LastRoze/STAAH-Publisher/raw/master/ZUZU-Menu.user.js
+// @icon         https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.deleteValue
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @run-at       document-start
+// @match        *://cp.staah.net/common-cgi/motels/admin/ChannelUpdate.pl*
+// @noframes
+// ==/UserScript==
