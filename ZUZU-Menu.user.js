59fd6d4c6eed713fc90a026df02b6057f9657aa9
Yoga <LastRoze@users.noreply.github.com>
2019-11-09 03:11:51 +0700

    Update ZUZU-Menu.user.js

--- ZUZU-Menu.user.js
+++ ZUZU-Menu.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         STAAH Publisher
-// @namespace    https://linkedin.com/in/lastroze
-// @author       Last Roze
-// @description  ZUZU Hospitality - STAAH publisher is created to easily publish rates across OTA with one click.
-// @icon         https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
-// @version      1.0
+// @namespace    STAAH Publisher
+// @description  ZUZU Hospitality - STAAH Publisher is created to easily publish rates across OTA with one click.
 // @copyright    ©2019 - Yoga Budiman
+// @version      1.1
+// @author       Last Roze
+// @homepageURL  https://github.com/LastRoze/STAAH-Publisher
+// @supportURL   https://linkedin.com/in/lastroze
+// @updateURL    https://github.com/LastRoze/STAAH-Publisher/raw/master/ZUZU-Menu.user.js
+// @downloadURL  https://github.com/LastRoze/STAAH-Publisher/raw/master/ZUZU-Menu.user.js
+// @icon         https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
 // @grant        GM_addStyle
 // @grant        GM_deleteValue
 // @grant        GM_getResourceURL
@@ -20,12 +24,10 @@
 // @grant        GM.openInTab
 // @grant        GM.xmlHttpRequest
 // @grant        unsafeWindow
+// @run-at       document-start
 // @match        *://cp.staah.net/common-cgi/motels/admin/ChannelUpdate.pl*
 // @noframes
 // ==/UserScript==
-
-var unlocker = document.getElementById('middleselfpass');
-unlocker.style.display = null;
 
 var oneClick = "<tr><td style=\"text-align: center;font-weight:700;\">ZUZU Menu</td></tr><tr><td style=\"text-align: center;\"> <input title=\"Update the rates for 30 days\" value=\"30 Days\" style=\"margin-right: 5px;\" type=\"button\" onclick=\"$('.btn-danger').click();\"><input title=\"Update the rates for 1 year\" value=\"Full Sync\" type=\"button\" onclick=\"$('.btn-gray').click();\"></td></tr>";
 $(oneClick).appendTo("thead");
